eaba750b3d8d0a681975e58cb17449edb785e03d
Alpaca <ayazhan1121@gmail.com>
2022-05-08 12:31:30 +0600

    feat: add check password validation

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser=require('body-parser')
 const mongoose = require('mongoose')
 const user = require ('./model/user')
 const bcrypt = require ('bcryptjs')
-mongoose.connect('mongodb://localhost:27017/login-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/login-db',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
 
 const app = express()
 
@@ -18,8 +19,36 @@ app.use(bodyParser.json())
 
 app.post('/api/register', async (req, res)=> {
     console.log(req.body)
-    const {barcode, first_name, last_name, password: plainTextPassword} = req.body
-    const password = await bcrypt.hash(password, 10)
+    const { barcode, first_name, last_name, password: plainTextPassword, password_confirm } = req.body
+    if(plainTextPassword.length < 7) {
+        return res.json({status:'error', error: 'Password too small. Should be at least 7 characters'})
+    }
+    if(plainTextPassword.search(/[a-z]/i) < 0) {
+        return res.json({status:'error', error: 'Your password must contain at least one small letter.'})
+    }
+    if(plainTextPassword.search(/[A-Z]/i) < 0) {
+        return res.json({status:'error', error: 'Your password must contain at least one capital letter.'})
+    }
+    if(plainTextPassword.search(/[0-9]/i) < 0) {
+        return res.json({status:'error', error: 'Your password must contain at least one digit.'})
+    }
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    try {
+        const response = await user.create({
+            barcode,
+            first_name,
+            last_name,
+            password,
+            password_confirm
+        })
+        console.log('User created successfully: ', response)
+    }
+    catch(error) {
+        if (error.code===11000) {
+        return res.json({status: 'error', error: 'Barcode is already in use'})
+        }
+        throw error
+    }
     res.json({status: 'ok'})
 })
 
