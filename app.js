e17a9441d458e0b4e28d4564fe62e72e920fbedb
Alpaca <ayazhan1121@gmail.com>
2022-05-08 11:22:14 +0600

    chore: add hash password using bcryptjs library

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require ('express')
 const bodyParser=require('body-parser')
 const mongoose = require('mongoose')
 const user = require ('./model/user')
+const bcrypt = require ('bcryptjs')
 mongoose.connect('mongodb://localhost:27017/login-db', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 const app = express()
@@ -17,6 +18,8 @@ app.use(bodyParser.json())
 
 app.post('/api/register', async (req, res)=> {
     console.log(req.body)
+    const {barcode, first_name, last_name, password: plainTextPassword} = req.body
+    const password = await bcrypt.hash(password, 10)
     res.json({status: 'ok'})
 })
 
